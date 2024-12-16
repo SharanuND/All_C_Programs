1233eff35f8e8af66d51d115f5c98bef87743070
SharanuND <sharanudibbadamani2001@gmail.com>
2024-12-16 17:35:33 +0530

    Renaming folder on-going

--- 1. Basics/3-print-alphabets-from-a-to-z-capital.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main(){
-    for(char i='a';i<='z';i++){
-        printf("%c ",i);
-    }
-}
--- 2. Numbers/1-sum-of-n-numbers.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    int n;printf("Enter the value of N : ");scanf("%d",&n);
-    int sum=0;
-    for(int i=1;i<=n;i++){
-        sum+=i;
-    }
-    printf("\nSum of N Numbers : %d",sum);
-}
